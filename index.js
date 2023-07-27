a8c265b8a791b758162b4fd7d45f23de5e82a520
Téa May Francinne <tmyfrncnn@gmail.com>
2023-07-27 16:14:07 +0800

    added animated favicon

--- index.js
+++ index.js
@@ -39,3 +39,22 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+let number = 1
+var favicon = document.getElementById("favicon");
+
+function swapFavicon() {
+    favicon.href = 'assets/favicon gif/0' + number + '.png' 
+
+    number++
+    if (number > 9) {
+        number = 1
+    }
+}
+
+// setInterval is a Javascript function that executes a 
+// function indefinite times (function is executed based on its seconds parameter())
+
+// Argument/Parameter #1 Function that you want to execute
+// Argument/Parameter #2 Time in milliseconds
+setInterval(swapFavicon, 1000)
