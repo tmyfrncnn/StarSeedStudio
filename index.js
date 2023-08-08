d465661bcf21a128e61f719c5e9ad6002f6f00d4
Téa May Francinne <tmyfrncnn@gmail.com>
2023-08-08 17:49:17 +0800

    changed contact detail design, made home logo transparent in mobile, added scrollbar design

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let number = 1
 var favicon = document.getElementById("favicon");
 
 function swapFavicon() {
-    favicon.href = 'assets/favicon gif/0' + number + '.png' 
+    favicon.href = 'assets/favicon gif/0' + number + '.png'
 
     number++
     if (number > 9) {
@@ -16,3 +16,22 @@ function swapFavicon() {
 // Argument/Parameter #1 Function that you want to execute
 // Argument/Parameter #2 Time in milliseconds
 setInterval(swapFavicon, 1000)
+
+// document.addEventListener("DOMContentLoaded", function () {
+// });
+const home = document.querySelector(".home");
+
+window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+    if (window.innerWidth <= 500) {
+        if (scrollPosition > 0) {
+            let newOpacityValue = 1 - (scrollPosition / 1000)
+            
+            home.style.opacity = newOpacityValue > 0.135
+            ? newOpacityValue
+            : 0.135
+        } else {
+            home.style.opacity = 1
+        }
+    }
+});
